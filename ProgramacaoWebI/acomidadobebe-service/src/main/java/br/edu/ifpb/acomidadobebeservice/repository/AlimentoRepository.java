49616bbd0ea24db3211f6f8a478bce3c4e4418e1
CleoniceSoares <cleonice.soares27cs@gmail.com>
2022-04-30 17:20:23 -0300

    Delete

--- ProgramacaoWebI/acomidadobebe-service/src/main/java/br/edu/ifpb/acomidadobebeservice/repository/AlimentoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.acomidadobebeservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifpb.acomidadobebeservice.model.Alimento;
-
-public interface AlimentoRepository extends JpaRepository<Alimento, Integer> {
-    
-}
--- ProgramacaoWebI/acomidadobebe-service/src/main/java/br/edu/ifpb/acomidadobebeservice/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.acomidadobebeservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifpb.acomidadobebeservice.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    
-}
